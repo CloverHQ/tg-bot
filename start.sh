c793e769db88b0b61a6b29bf9959391f8f34547d
mimikatz <775856746@qq.com>
2022-08-14 01:33:51 +0800

    修改目录

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
 docker run -d -p 8080:80 -v /nextcloud/apps:/var/www/html/custom_apps -v /nextcloud/config:/var/www/html/config -v /nextcloud/data:/var/www/html/data  --privileged=true nextcloud    
+
+docker run -d -v /nextcloud/data/tg_bot/files/tg_video:/data/down -v/root/tg-bot:/var/app --privileged=true tg-bot/v1
